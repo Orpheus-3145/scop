0a6035eab55741b876d9ed996f256eada5a11e16
Fra <francesco.aru25@gmail.com>
2025-11-18 19:07:56 +0100

    first commit, includes ok

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+
+
+int main(int argc, char** argv) {
+	(void) argc; (void) argv;
+	std::cout << "program ok!\n";
+	return (EXIT_SUCCESS);
+}
