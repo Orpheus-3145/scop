79fc0ee8f126807907a26fb6634b2be1932d7f31
Fra <francesco.aru25@gmail.com>
2025-11-25 18:48:22 +0100

    added some openGL features

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,17 @@
 
 
 void pressEscCb(GLFWwindow* window, int key, int scancode, int action, int mods) {
-	(void)scancode; (void)mods;
+	(void) scancode; (void) mods;
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
+
+void resizeCb(GLFWwindow* window, int width, int height)
+{
+	(void) window;
+    glViewport(0, 0, width, height);
+}
+
 
 int main(int argc, char** argv) {
 	(void) argc; (void) argv;
@@ -23,8 +30,11 @@ int main(int argc, char** argv) {
 		std::cout << "initialization of glfw failed: " << description << std::endl;	
 		return (EXIT_FAILURE);
 	}
-	std::cout << "glfw initialized, using version: " << GLFW_CONTEXT_VERSION_MAJOR << "." << GLFW_CONTEXT_VERSION_MINOR << std::endl;
+	std::cout << "glfw initialized, using version: " << GLFW_CONTEXT_VERSION_MAJOR << "." << GLFW_CONTEXT_VERSION_MINOR << "." << GLFW_VERSION_REVISION << std::endl;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	GLFWwindow* newWindow = glfwCreateWindow(1920, 1080, "test win", nullptr, nullptr);
 	if (!newWindow) {
 		const char* description;
@@ -41,11 +51,17 @@ int main(int argc, char** argv) {
 
 	glfwMakeContextCurrent(newWindow);
 	gladLoadGL();
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	// callbacks
 	glfwSetKeyCallback(newWindow, pressEscCb);
+	glfwSetFramebufferSizeCallback(newWindow, resizeCb);
 
+	// render loop
 	while (!glfwWindowShouldClose(newWindow)) {
-		// glfwSwapBuffers(newWindow);
+		glClearColor(0.8f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 		glfwPollEvents();
+		glfwSwapBuffers(newWindow);
 	}
 	
 	glfwDestroyWindow(newWindow);
