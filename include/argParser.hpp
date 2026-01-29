bd4661e6b46e7072a5d33aa2e6f955ab09ac02b7
Francesco <francesco1896@gmail.com>
2026-01-29 03:57:54 +0100

    created module for arg parse

---
+++ include/argParser.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include <map>
+#include <cstdint>
+#include <optional>
+#include <functional>
+#include <string>
+
+#include "exceptions.hpp"
+#include "define.hpp"
+
+
+constexpr const char* HOW_TO = R"(Usage: ./scop [options]
+Program that renders an obj file (see for reference: https://en.wikipedia.org/wiki/Wavefront_.obj_file)
+Directives supported: v, vt, vn, f. Materials and lights not implemented yet.
+
+	-f,  --file             object file (e.g. generated from Blender) to render
+	-w,  --width            width in pixel of the window
+	-h,  --height           height in pixel of the window
+	-vs, --vertexShader     file for vertex shader
+	-ts, --textureShade     file for fragment shader
+	-t,  --texture          texture file to apply to the object
+	     --help             print info
+
+	[options can be set with next word or = : --opt value  | --opt=value ]
+)";
+
+struct InputData {
+	std::string objFile = SCOP_OBJECT_FILE;
+	uint32_t	width = SCOP_WINDOW_WIDTH;
+	uint32_t	height = SCOP_WINDOW_HEIGHT;
+	std::string vertexShaderFile = SCOP_VERTEX_SHADER;
+	std::string fragmentShaderFile = SCOP_FRAGMENT_SHADER;
+	std::string textureFile = SCOP_TEXTURE_CAPYBARA;
+	bool		helpmode = false;
+
+    static InputData    parseArgs( int32_t, char** ) ;
+	static void         setObjectFile( InputData&, std::optional<std::string> );
+	static void         setWidth( InputData&, std::optional<std::string> );
+	static void         setHeight( InputData&, std::optional<std::string> );
+	static void         setVertexShaderOpt( InputData&, std::optional<std::string> );
+	static void         setFragmentShaderOpt( InputData&, std::optional<std::string> );
+	static void         setTextureFile( InputData&, std::optional<std::string> );
+	static void         setHelpMode( InputData&, std::optional<std::string> );
+};
+
+enum class OptionType {
+    ObjectFile,
+    Width,
+    Height,
+    VertexShaderFile,
+    TextureShaderFile,
+    TextureFile,
+    Helpmode
+};
+
+const std::map<std::string, OptionType> flagMap = {
+	{"-f", OptionType::ObjectFile},
+	{"--file", OptionType::ObjectFile},
+	{"-w", OptionType::Width},
+	{"--width", OptionType::Width},
+	{"-h", OptionType::Height},
+	{"--height", OptionType::Height},
+	{"-vs", OptionType::VertexShaderFile},
+	{"--vertexShader", OptionType::VertexShaderFile},
+	{"-ts", OptionType::TextureShaderFile},
+	{"--textureShader", OptionType::TextureShaderFile},
+	{"-t", OptionType::TextureFile},
+	{"--texture", OptionType::TextureFile},
+	{"--help", OptionType::Helpmode}
+};
+
+const std::map<OptionType, std::function<void( InputData&, std::optional<std::string> )>> flagActions = {
+	{OptionType::ObjectFile, InputData::setObjectFile},
+	{OptionType::Width, InputData::setWidth},
+	{OptionType::Height, InputData::setHeight},
+	{OptionType::VertexShaderFile, InputData::setVertexShaderOpt},
+	{OptionType::TextureShaderFile, InputData::setFragmentShaderOpt},
+	{OptionType::TextureFile, InputData::setTextureFile},
+	{OptionType::Helpmode, InputData::setHelpMode},
+};
---
+++ src/argParser.cpp
@@ -0,0 +1,87 @@
+#include "argParser.hpp"
+
+void InputData::setObjectFile( InputData& input, std::optional<std::string> optValue ) {
+    if (optValue.has_value() == false)
+        throw ParsingException("Missing value for --file/-f");
+    input.objFile = optValue.value();
+}
+
+void InputData::setWidth( InputData& input, std::optional<std::string> optValue ) {
+    try {
+        input.width = std::stoi(optValue.value());
+    } catch (std::bad_optional_access const&) {
+        throw ParsingException("Missing value for --width/-w");
+    } catch (std::invalid_argument const&) {
+        throw ParsingException("Wrong number input: " + optValue.value());
+    } catch (std::out_of_range const&) {
+        throw ParsingException("Out of range: " + optValue.value());
+    }
+}
+
+void InputData::setHeight( InputData& input, std::optional<std::string> optValue ) {
+    try {
+        input.height = std::stoi(optValue.value());
+    } catch (std::bad_optional_access const&) {
+        throw ParsingException("Missing value for --height/-h");
+    } catch (std::invalid_argument const&) {
+        throw ParsingException("Wrong number input: " + optValue.value());
+    } catch (std::out_of_range const&) {
+        throw ParsingException("Out of range: " + optValue.value());
+    }
+}
+
+void InputData::setVertexShaderOpt( InputData& input, std::optional<std::string> optValue ) {
+    if (optValue.has_value() == false)
+        throw ParsingException("Missing value for --vertexShader/-vs");
+    input.vertexShaderFile = optValue.value();
+    
+}
+
+void InputData::setFragmentShaderOpt( InputData& input, std::optional<std::string> optValue ) {
+    if (optValue.has_value() == false)
+        throw ParsingException("Missing value for --fragmentShader/-fs");
+    input.fragmentShaderFile = optValue.value();
+    
+}
+
+void InputData::setTextureFile( InputData& input, std::optional<std::string> optValue ) {
+    if (optValue.has_value() == false)
+        throw ParsingException("Missing value for --texture/-t");
+    input.textureFile = optValue.value();
+}
+
+void InputData::setHelpMode( InputData& input, std::optional<std::string> optValue ) {
+    (void)optValue;
+    input.helpmode = true;
+}
+
+
+InputData InputData::parseArgs(int32_t argc, char* argv[]) {
+    InputData opts;
+	std::string arg, value;
+	OptionType option;
+
+    for (int32_t i = 1; i < argc; ++i) {
+		arg = argv[i];
+
+		// support for: argoment=value
+        size_t eqPos = arg.find('=');
+        if (eqPos != std::string::npos) {		// if it is: --key=value
+			if (eqPos == 0 or eqPos == arg.size() - 1)
+				throw ParsingException("invalid argument with value: " + arg);
+            value = arg.substr(eqPos + 1);
+            arg = arg.substr(0, eqPos);
+        } else if ((i + 1 < argc) and (argv[i + 1][0] != '-'))		// or is: --key value
+            value = argv[++i];
+
+        auto mapOption = flagMap.find(arg);
+        if (mapOption == flagMap.end())
+            throw ParsingException("Unknown argument: " + arg);
+		option = mapOption->second;
+
+		auto it = flagActions.find(option);
+        if (it != flagActions.end())
+            it->second(opts, value);	// run action depending on option type
+	}
+    return opts;
+}
